9f37058353bf8b4aff7cc09a1b436406d1da602a
mattmatters <domattthings@gmail.com>
2018-06-14 22:39:04 -0700

    Call deploy from project root

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,12 @@
 NAME="face_replace"
 
 pip install awscli
-aws s3 cp . $BUCKET --recursive --exclude *.wasm --exclude *.data
+aws s3 cp web $BUCKET --recursive --exclude *.wasm --exclude *.data
 
 # The aws cli guesses wrong for this file
-aws s3 cp $NAME.wasm $BUCKET --content-type "application/wasm"
+aws s3 cp web/$NAME.wasm $BUCKET --content-type "application/wasm"
 
 # Compress data file
-gzip -9 $NAME.data
-mv $NAME.data.gz $NAME.data
-aws s3 cp $NAME.data $BUCKET --content-encoding "gzip"
+gzip -9 web/$NAME.data
+mv web/$NAME.data.gz web/$NAME.data
+aws s3 cp web/$NAME.data $BUCKET --content-encoding "gzip"
