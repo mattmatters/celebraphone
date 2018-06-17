c1a7b74907f1f0fbab835a51b3fe86f9ee5c9d31
mattmatters <domattthings@gmail.com>
2018-06-17 01:13:41 -0700

    Upload title has been changed to files name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,9 @@ pip install awscli
 aws s3 cp dist $BUCKET --recursive --exclude *.wasm --exclude *.data
 
 # The aws cli guesses wrong for this file
-aws s3 cp dist/wasm/$NAME.wasm $BUCKET/wasm --content-type "application/wasm"
+aws s3 cp dist/wasm/$NAME.wasm $BUCKET/wasm/$NAME.wasm --content-type "application/wasm"
 
 # Compress data file
 gzip -9 dist/wasm/$NAME.data && \
   mv dist/wasm/$NAME.data.gz dist/wasm/$NAME.data
-aws s3 cp dist/wasm/$NAME.data $BUCKET/wasm --content-encoding "gzip"
+aws s3 cp dist/wasm/$NAME.data $BUCKET/wasm/$NAME.data --content-encoding "gzip"
